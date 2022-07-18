d0d79313e82b5abe3d78672e94a7fac041495cfe
nvn-laufire <nvn@laufire.in>
2022-07-18 20:08:18 +0530

    bug fixed

--- src/components/StudentData/snackBar.js
+++ src/components/StudentData/snackBar.js
@@ -1,19 +1,16 @@
 import { React } from 'react';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
-import { Tooltip } from '@mui/material';
 import DoneIcon from '@mui/icons-material/Done';
 
 const vertical = 'top';
 const horizontal = 'center';
 
 const action = () =>
-	<Tooltip title="close">
-		<Button
-			variant="contained"
-			color="success"
-		><DoneIcon/> </Button>
-	</Tooltip>;
+	<Button
+		variant="contained"
+		color="success"
+	><DoneIcon/> </Button>;
 
 const AlertMessage = (context) => {
 	const { state: { Alert }} = context;
@@ -28,7 +25,7 @@ const AlertMessage = (context) => {
 			key={ vertical + horizontal }
 			anchorOrigin={ { vertical, horizontal } }
 			open={ Alert }
-			autoHideDuration={ 6000 }
+			autoHideDuration={ 2 }
 			onClose={ () => context.actions.editAlert() }
 			message="Successfully Saved"
 			action={ action() }
