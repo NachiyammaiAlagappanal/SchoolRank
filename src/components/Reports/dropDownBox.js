ffca8aa25e6531393a994222bfaeda8de0b06604
nvn-laufire <nvn@laufire.in>
2022-07-21 12:19:23 +0530

    select using Material UI

--- src/components/Reports/dropDownBox.js
+++ src/components/Reports/dropDownBox.js
@@ -1,18 +1,24 @@
 import { React } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 const DropDownBox = (context) => {
 	const { state } = context;
 
-	return	<div>
-		<select
-			id="dropdown-list"
-			value={ state.selectedChart }
-			onChange={ (evt) =>
-				context.actions.setChart(evt.target.value) }
-		>
-			<option value="HeatMap"> HeatMap </option>
-			<option value="PieChart"> PieChart</option>
-		</select>
+	return	<div style={ { padding: 20 } }>
+		<FormControl fullWidth={ true }>
+			<InputLabel>ChartTypes</InputLabel>
+			<Select
+				id="dropdown-list"
+				value={ state.selectedChart }
+				label="ChartTypes"
+				autoWidth={ true }
+				onChange={ (evt) =>
+					context.actions.setChart(evt.target.value) }
+			>
+				<MenuItem value="HeatMap"> HeatMap </MenuItem>
+				<MenuItem value="PieChart"> PieChart</MenuItem>
+			</Select>
+		</FormControl>
 	</div>;
 };
 
