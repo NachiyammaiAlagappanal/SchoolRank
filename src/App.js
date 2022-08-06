ba011383a062cef99cb9aec7e4ae5af3974cf88a
nvn-laufire <nvn@laufire.in>
2022-08-06 21:23:59 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { React } from 'react';
 import './App.scss';
-import TabContent from './components/TabContent';
 import { Box } from '@mui/material';
+import Header from './components/Header';
+import MenuBar from './components/MenuBar';
+import TabPanel from './components/TabPanel';
+import Data from './components/Data';
+import Reports from './components/Reports';
 
-const App = (context) =>
-	<Box className="App" role="App">
-		<TabContent { ...context }/>
+const App = (context) => {
+	const { state: { value }} = context;
+
+	return <Box className="App" role="App">
+		<Box sx={ { display: 'flex' } }>
+			<MenuBar { ...context }/>
+			<Box width="100%">
+				<Header/>
+				<TabPanel value={ value } index={ 0 }>
+					<Data { ...context }/>
+				</TabPanel>
+				<TabPanel value={ value } index={ 1 }>
+					<Reports { ...context }/>
+				</TabPanel>
+			</Box>
+		</Box>
 	</Box>;
+};
 
 export default App;
